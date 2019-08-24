0485e0170a07735aa8eb80d6c157ab2b6579d373
Christina Labay <Christina.m.labay@gmail.com>
2019-08-24 13:18:10 -0500

    input output exercise

---
+++ inputOutput.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// Task: 
+// Read  numbers from stdin and print their sum to stdout.
+
+int main() { 
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+    return 0;
+}
